39aea82f6562e9816731a951052516f931ed9031
devkbm <devkbm0417@gmail.com>
2022-12-09 10:56:25 +0900

    first commit

---
+++ src/main/java/com/like/JsonDateTimeExampleApplication.java
@@ -0,0 +1,13 @@
+package com.like;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonDateTimeExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonDateTimeExampleApplication.class, args);
+	}
+
+}
